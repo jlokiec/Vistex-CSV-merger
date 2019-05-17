b6999f7b384efe3277331a7af55497ca81d0ebf1
Jakub Łokieć <jakublokiec@wp.pl>
2019-05-17 16:25:04 +0200

    Create class for reading CSV files

---
+++ CsvReader/CsvReader.cs
@@ -0,0 +1,59 @@
+﻿using CsvHelper.Configuration;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CsvReader
+{
+    public class CsvReader
+    {
+        private static string CSV_DELIMITER = ",";
+
+        private List<string> dimensionColumns;
+
+        public CsvReader(List<string> dimensionColumns)
+        {
+            this.dimensionColumns = dimensionColumns;
+        }
+
+        public List<CsvEntry> ReadFile(string fileName)
+        {
+            var csvEntries = new List<CsvEntry>();
+            var csvReaderConfiguration = new Configuration()
+            {
+                Delimiter = CSV_DELIMITER
+            };
+
+            using (var streamReader = File.OpenText(fileName))
+            using (var csvReader = new CsvHelper.CsvReader(streamReader, csvReaderConfiguration))
+            {
+                csvReader.Read();
+                csvReader.ReadHeader();
+                var header = csvReader.Context.HeaderRecord;
+
+                while (csvReader.Read())
+                {
+                    var csvEntry = new CsvEntry();
+
+                    foreach (var name in header)
+                    {
+                        bool isDimension = false;
+
+                        if (dimensionColumns.Contains(name))
+                        {
+                            isDimension = true;
+                        }
+
+                        var value = csvReader.GetField(name);
+                        csvEntry.AddValue(name, value, isDimension);
+                    }
+
+                    csvEntries.Add(csvEntry);
+                }
+            }
+
+            return csvEntries;
+        }
+    }
+}
