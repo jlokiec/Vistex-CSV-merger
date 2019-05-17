99c78f1c0a9de9ef36cc3f52cf64192f3138c6c5
Jakub Łokieć <jakublokiec@wp.pl>
2019-05-17 14:14:17 +0200

    Create base VS project

---
+++ Vistex-CSV-merger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vistex_CSV_merger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
