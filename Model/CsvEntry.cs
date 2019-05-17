d1edd8ad0494ff7fee321f7abcf7ea8a59be9719
Jakub Łokieć <jakublokiec@wp.pl>
2019-05-17 16:19:41 +0200

    Create model for CSV entry
    It contains 2 dictionaries: one for dimensions and one for measurements.
    Dimensions are treated as keys for CsvEntry object and Measurements are
    the object's values.

---
+++ Model/CsvEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class CsvEntry
+    {
+        public Dictionary<string, string> DimensionValues { get; private set; }
+        public Dictionary<string, string> MeasureValues { get; private set; }
+
+        public CsvEntry()
+        {
+            DimensionValues = new Dictionary<string, string>();
+            MeasureValues = new Dictionary<string, string>();
+        }
+
+        public void AddValue(string key, string value, bool isDimension)
+        {
+            if (isDimension)
+            {
+                DimensionValues.Add(key, value);
+            }
+            else
+            {
+                MeasureValues.Add(key, value);
+            }
+        }
+
+        public override string ToString()
+        {
+            var s = string.Join(',', DimensionValues.Values);
+            s += ',';
+            s += string.Join(',', MeasureValues.Values);
+            return s;
+        }
+    }
+}
